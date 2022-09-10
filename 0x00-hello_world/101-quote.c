f934284725e96aa29ad1863d628175a71b3f01ec
Oluwafemi <famousy25@gmail.com>
2022-09-10 04:40:28 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,8 @@
 #include <unistd.h>
 /**
-
  *  main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *
- *                   followed by a new line, to standard error.
- *
- *    
- *
- *      Return: Always 1.
+ * followed by a new line, to standard error.   
+ * Return: Always 1.
  */
 int main(void)
 
@@ -22,5 +17,4 @@ int main(void)
 
 
 	  return (1);
-
 }
