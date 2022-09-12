42ef1420f884f54e49beefb83278542168a90b98
Oluwafemi <famousy25@gmail.com>
2022-09-12 14:58:55 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+#include <stdlib.h>
+
+/**
+ *  main - entry point
+ *  Return: 0
+ */
+
+int main(void)
+
+{
+
+	char ch = '0';
+
+	int i = 0;
+
+	/*input  your code goes there */
+
+	while (i < 10)
+
+	{
+
+	putchar(ch);
+
+	ch++;
+
+	i++;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
