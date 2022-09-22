2d656232c83950c47df5e7f92b5bf0e95117cf54
Famossy <111335573+Famossy@users.noreply.github.com>
2022-09-23 00:06:54 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,10 +13,10 @@ int _strcmp(char *s1, char *s2)
 
 	for (n = 0; s1[n] != '\0'; n++)
 	{
-		if (s1[n] > s2[n])
-			return (s1[n] - s2[n]);
-		if (s2[n] > s1[n])
-			return (s1[n] - s2[n]);
+	if (s1[n] > s2[n])
+	return (s1[n] - s2[n]);
+	if (s2[n] > s1[n])
+	return (s1[n] - s2[n]);
 	}
 	return (0);
 }
